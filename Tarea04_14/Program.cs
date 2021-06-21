298e2a4b79e2a3ca120d948afc232af2031fb9b4
Carlos <carlos@xtrene.com>
2021-06-21 23:34:10 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tarea04_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el radio:  ");
+            double radio = Convert.ToDouble( Console.ReadLine() );
+
+            Console.WriteLine("El área del círculo es:  {0}", Math.PI*Math.Pow(radio,2) );
+
+            Console.ReadKey();
+        }
+    }
+}
